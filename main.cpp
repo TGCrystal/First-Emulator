5908c8c17a62e2bee4e35328c6249ee7677d93e5
Trevor <TGCrystal@users.noreply.github.com>
2020-03-13 00:09:13 -0400

    Added support for command line arguments for file selection and disassembler selection

--- main.cpp
+++ main.cpp
@@ -5,16 +5,26 @@
 
 int main(int argc, char* argv[]) {
 
-	const std::string fileName = "cpudiag.bin";
+	if(argc < 2 || argc > 3) {
+		std::cerr << "Incorrect number of arguments" << std::endl;
+		exit(1);
+	}
+
+	const std::string fileName = argv[1];
 
 	MachineState state(fileName);
-	state.printDisassembled();
 
-	// while(!state.isDone()) {
-	// 	state.printState();
-	// 	while (std::cin.get() != '\n');
-	// 	state.processCommand();
-	// }
+	if(argc == 3 && (std::string) argv[2] == "-d") {
+		state.printDisassembled();
+	}
+
+	else {
+		while(!state.isDone()) {
+			state.printState();
+			while (std::cin.get() != '\n');
+			state.processCommand();
+		}
+	}
 
 	return 0;
 
